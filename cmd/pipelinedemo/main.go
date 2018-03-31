423cf17cf6d19a151eefa3caf3c3ad7a23731e30
yueyt <yyt030@gmail.com>
2018-03-31 15:20:34 +0800

    3-3

--- cmd/pipelinedemo/main.go
+++ cmd/pipelinedemo/main.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
-	p := pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4))
+	p := pipeline.Merge(
+		pipeline.InMemSort(pipeline.ArraySource(3, 2, 16, 7, 4)),
+		pipeline.InMemSort(pipeline.ArraySource(13, 2, 6, 17, 4)))
+
 	for v := range p {
 		fmt.Println(v)
 	}
-
 }
