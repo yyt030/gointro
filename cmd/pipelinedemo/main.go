e6d463bc91c8d446494c055a502aa5cde7f77d88
yueyt <yyt030@gmail.com>
2018-04-20 21:41:04 +0800

    another pipeline demo

--- cmd/pipelinedemo/main.go
+++ cmd/pipelinedemo/main.go
@@ -1,49 +1,75 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-
-	"gointro/pipeline"
+	"sync"
 )
 
-func main() {
-	const filename = "small.in"
-	const num = 64
-	file, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	p := pipeline.RandomSource(num)
-	writer := bufio.NewWriter(file)
-	pipeline.WriterSink(writer, p)
-	writer.Flush()
-
-	file, err = os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	p = pipeline.ReaderSource(file, -1)
-
-	count := 0
-	for v := range p {
-		fmt.Println(v)
-		count++
-		if count > 100 {
-			break
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
 		}
-	}
+		close(out)
+	}()
+
+	return out
 }
 
-func mergeDemo() {
-	p := pipeline.Merge(
-		pipeline.InMemSort(pipeline.ArraySource(3, 2, 16, 7, 4)),
-		pipeline.InMemSort(pipeline.ArraySource(13, 2, 6, 17, 4)))
-	for v := range p {
-		fmt.Println(v)
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+
+	return out
+}
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func main() {
+	// Set pipeline
+	nums := []int{2, 3, 4, 5, 6}
+	c := gen(nums...)
+	out := sq(c)
+
+	for n := range out {
+		fmt.Println("demo1", n)
+	}
+
+	for n := range sq(gen(nums...)) {
+		fmt.Println("demo2", n)
+	}
+
+	c1 := sq(gen(nums...))
+	c2 := sq(gen(nums...))
+
+	for n := range merge(c1, c2) {
+		fmt.Println("demo3", n)
 	}
 }
